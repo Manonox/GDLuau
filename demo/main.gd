bd260d3f7815433be4b923d59e5e7956530039b5
SDFTDusername <85052868+SDFTDusername@users.noreply.github.com>
2024-07-28 16:34:27 +0200

    Update main.gd to work with my other changes

--- demo/main.gd
+++ demo/main.gd
@@ -2,26 +2,25 @@ extends Node
 
 
 @onready var vm : LuauVM = $LuauVM
-@export var open_all_libraries := true
 
 func _ready():
 	print("================")
 
-	if open_all_libraries:
-		vm.open_all_libraries()
-	else:
-		vm.open_libraries([
-			"", # Base library, contains functions like print and tostring
-			"coroutine",
-			"table",
-			"string",
-			"utf8",
-			"math",
-			"os",
-			"bit32",
-			"buffer",
-			"debug"
-		])
+	vm.open_all_libraries()
+	
+	# You can also open libraries like this
+	#vm.open_libraries([
+		#vm.LUA_LIB_BASE,
+		#vm.LUA_LIB_COROUTINE,
+		#vm.LUA_LIB_TABLE,
+		#vm.LUA_LIB_OS,
+		#vm.LUA_LIB_STRING,
+		#vm.LUA_LIB_MATH,
+		#vm.LUA_LIB_VECTOR,
+		#vm.LUA_LIB_DEBUG,
+		#vm.LUA_LIB_UTF8,
+		#vm.LUA_LIB_BIT32
+	#])
 	
 	vm.lua_pushobject(self)
 	vm.lua_setfield(vm.LUA_GLOBALSINDEX, "node")
