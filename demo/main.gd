42af1aad8de4a3ac9c64c09d57d7b17b6da9de27
Manonox <tigranmamedov2002@gmail.com>
2025-01-07 23:21:24 +0300

    🧪 Improved demo code (it works now)

--- demo/main.gd
+++ demo/main.gd
@@ -2,38 +2,41 @@ extends Node
 
 
 @onready var vm : LuauVM = $LuauVM
-
 func _ready():
-	print("================")
-
 	vm.open_all_libraries()
 	
-	# You can also open libraries like this
-	#vm.open_libraries([
-		#vm.LUA_BASE_LIB,
-		#vm.LUA_COROUTINE_LIB,
-		#vm.LUA_TABLE_LIB,
-		#vm.LUA_OS_LIB,
-		#vm.LUA_STRING_LIB,
-		#vm.LUA_MATH_LIB,
-		#vm.LUA_VECTOR_LIB,
-		#vm.LUA_DEBUG_LIB,
-		#vm.LUA_UTF8_LIB,
-		#vm.LUA_BIT32_LIB
-	#])
-	
-	vm.lua_pushobject(self)
-	vm.lua_setfield(vm.LUA_GLOBALSINDEX, "node")
-
+	vm.stdout.connect(_stdout)
+	# Puts the number on top of the stack
+	vm.lua_pushnumber(3)
+	# Pops the top of the stack (our number) and sets a global variable "x"
+	vm.lua_setglobal("x")
+	# Compiles and executes a string of lua code
 	var success := vm.lua_dostring("""
-		node.shit = 3
+		local t = {
+			shit = 3,
+		}
+		
+		print("t = ", t)
+		print("t.shit = ", t.shit)
+		
+		y = x + t.shit
 	""")
-	
+
 	if success != vm.LUA_OK:
+		# An error happened, pop an error string off the stack and print it to godot console
 		print(vm.lua_tostring(-1))
 		vm.lua_pop()
 		return
-	
 
-func _on_luau_vm_stdout(message):
-	print(message)
+	# Put the value of a global variable "y" on top of the stack
+	vm.lua_getglobal("y")
+	# "Convert" a lua number on top of the stack to a godot float
+	var result := vm.lua_tonumber(-1)
+	# Clear the stack (stack only has a single number in it)
+	vm.lua_pop()
+
+	print("Result (y) = %s" % result)
+
+
+func _stdout(message: String) -> void:
+	print("Lua says: %s" % message)
